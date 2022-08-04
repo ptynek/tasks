6c6733d7d50296c2a157561d54d58747c3968ba5
Piotr Tynek <tynek.piotr@gmail.com>
2022-08-04 19:35:12 +0200

    Task 23.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,4 +12,17 @@ public class CoreConfiguration {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi(){
+        String pathsToMatch[] = {"/v1/tasks/**"};
+        String packagesToScan[] = {"com.crud.tasks.controller"};
+        return GroupedOpenApi.builder()
+                .group("tasks")
+                .pathsToMatch(pathsToMatch)
+                .packagesToScan(packagesToScan)
+                .build();
+    }
+
+
 }
